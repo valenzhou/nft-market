d2b20d878441673b0fdba557eae771b75517b824
Wen Zhou <wenzhou@WenMacBook-Pro-M3.local>
2024-08-23 11:40:10 +0800

    delete: remove useless contract file

--- my_contract/ignition/modules/BYBY.ts
+++
@@ -1,8 +0,0 @@
-import {buildModule} from "@nomicfoundation/hardhat-ignition/modules";
-
-const BybyModule = buildModule("BybyModule",(m) => {
-    const byby = m.contract("BYBY",[]);
-    return {byby}
-});
-
-export default BybyModule;
--- my_contract/ignition/modules/BYBYNft.ts
+++
@@ -1,8 +0,0 @@
-import {buildModule} from "@nomicfoundation/hardhat-ignition/modules";
-
-const BybyNftModule = buildModule("BybyNftModule",(m) => {
-    const bybyNft = m.contract("BYBYNft",[]);
-    return {bybyNft}
-});
-
-export default BybyNftModule;
